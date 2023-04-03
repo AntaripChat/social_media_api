02c92e2b975110ad223475c8fe93c088ee2162d9
Antarip Chatterjee <chatterjeeantarip1234@gmail.com>
2023-04-03 10:33:58 +0530

    change somthing

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ const app = require('./app');
 const config = require('./config');
 const {PORT} = require('./config/index');
 require('./config/db.config')
+
+app.get('/',(req,res)=>{
+    res.send('hello')
+})
 // mongo.connect('mongodb+srv://social_media:yDWn6wCnWJI73J9p@cluster0.0ypsb62.mongodb.net/?retryWrites=true&w=majority')
 // .then(()=>console.log('DB connect'))
 // .catch((err)=>console.log(err));
